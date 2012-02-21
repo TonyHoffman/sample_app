86b95177c6f44b300c98563d960b95539cb921c2
Tony Hoffman <thetonyhoffman@gmail.com>
2012-02-21 11:42:53 -0500

    Handling invalid signin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  include SessionsHelper #this makes the sessions_helper accessible outside the views (defaults to only being available in views)
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,37 @@
 module SessionsHelper
+  
+  def sign_in(user)
+    cookies.permanent.signed[:remember_token] = [user.id, user.salt]
+    current_user = user
+    
+    
+  end
+  
+  def current_user=(user)
+    @current_user = user
+    
+  end
+  
+  def current_user
+    @current_user ||= user_from_remember_token
+    # above: if current user is nil, then assign user_from_remember_token, second time is true so won't hit the database
+  end
+  
+  def signed_in?
+    !current_user.nil? #returns true if user is signed in
+  end
+  
+  private
+  
+    def user_from_remember_token
+      User.authenticate_with_salt(*remember_token)
+      #Above, * unwraps the remember_token to get at the two things inside the array
+      
+    end
+  
+  def remember_token
+    cookies.signed[:remember_token] || [nil, nil]
+  end
+  
+  
 end
