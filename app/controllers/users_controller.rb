ec1fab695a26b0c972c92054f7c8f611fe9bccf6
Tony Hoffman <thetonyhoffman@gmail.com>
2012-02-25 11:59:15 -0500

    Editing and updating users.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,5 +22,20 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
+    @user = User.find(params[:id])
+    @title = "Edit user"
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      # it worked
+      redirect_to @user, :flash => {:success => "Profile updated!"}
+    else
+      @title = 'Edit user'
+      render 'edit'
+  end
+end
 
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -39,9 +39,7 @@ describe UsersController do
       response.should have_selector('td>a', :content => user_path(@user),
                                             :href   => user_path(@user))
     end 
-    
   end
-    
     
   
   describe "GET 'new'" do
@@ -108,11 +106,86 @@ describe UsersController do
       it "should sign the user in" do
         post :create, :user => @attr
         controller.should be_signed_in
-      end
-                  
+      end        
     end
   end
 
+  describe "Get 'edit'" do
+    
+    before(:each) do
+      @user = Factory(:user)
+      test_sign_in(@user)
+    end
+    
+    it "should be successful" do
+      get :edit, :id => @user
+      response.should be_success
+    end
+    
+    it "should have the right title" do
+       get :edit, :id => @user
+       response.should have_selector('title', :content => "Edit user")
+     end
+    
+    it "should have a link to change the gravatar" do
+      get :edit, :id => @user
+      response.should have_selector('a', :href => 'http://gravatar.com/emails',
+                                        :content => "change")
+    end
+    
+  end
+
+ describe "Put 'update'" do
+   
+   before(:each) do
+      @user = Factory(:user)
+      test_sign_in(@user)
+      end
+      
+   describe "failure" do
+     
+     before(:each) do
+       @attr = { :name => "", :email => "", :password => "", 
+                  :password_confirmation => ""}
+      end
+      
+      it "should render the 'edit' page" do
+        put :update, :id => @user, :user => @attr
+        response.should render_template('edit') 
+      end
+     
+     it "should have the right title" do
+       put :update, :id => @user, :user => @attr
+       response.should have_selector('title', :content => "Edit user")
+      end
+    end
+ 
+ describe "success" do
+   
+   before(:each) do
+     @attr = { :name => "New Name", :email => "newemail@example.org",
+                :password => "barbaz", :password_confirmation => "barbaz" }
+    end
+
+    it "should change the user's attributes" do
+      put :update, :id => @user, :user => @attr
+      user = assigns(:user)
+      @user.reload
+      @user.name.should == user.name
+      @user.email.should == user.email
+      @user.encrypted_password.should == user.encrypted_password
+  end
+  
+  it "should have a flash message" do
+    put :update, :id => @user, :user => @attr
+    flash[:success].should =~ /updated/
+  end
+    
+    
+ 
+ end
+
+   end 
 end
 
 
