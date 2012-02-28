41121ea6cdec55f5fb780069a94645101dbec69d
Tony Hoffman <thetonyhoffman@gmail.com>
2012-02-27 22:02:44 -0500

    Can now destroy users(as admins) but have a problem with the test that for disallowing admin destruction of self, although works in test.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
   
-  before_filter :authenticate, :only => [:edit, :update, :index]
+  before_filter :authenticate, :only => [:edit, :update, :index, :destroy]
   before_filter :correct_user, :only => [:edit, :update]
+  before_filter :admin_user, :only => :destroy
   
   def index
     @users = User.paginate(:page => params[:page])
@@ -23,7 +24,7 @@ class UsersController < ApplicationController
     @user = User.new(params[:user])
     if @user.save
       sign_in @user
-      redirect_to @user, :flash => {:success => "Welcome to the Sample App!"}
+      redirect_to @user, :flash => {:success => "Welcome to the Sample App!"} 
     else
       @title = 'Sign up'
       render 'new' 
@@ -37,8 +38,7 @@ class UsersController < ApplicationController
   end
   
   def update
-    @user = User.find(params[:id])
-    if @user.update_attributes(params[:user])
+    if @user.update_attributes(params[:user]) 
       # it worked
       redirect_to signin_path, :flash => {:success => "Profile updated!"}
     else
@@ -46,6 +46,11 @@ class UsersController < ApplicationController
       render 'edit'
   end
 end
+
+  def destroy
+    User.find(params[:id]).destroy
+    redirect_to users_path, :flash => { :success => "User destroyed."}
+  end
 
   private
   
@@ -56,6 +61,11 @@ end
   def correct_user
     @user = User.find(params[:id])
     redirect_to(root_path) unless current_user?(@user)
+  end
+  
+  def admin_user
+    user = User.find(params[:id])
+    redirect_to(root_path) if !current_user.admin? || current_user?(user) 
   end
   
   
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -137,6 +137,37 @@ describe User do
       matching_user.should == @user
     end
   end
+  
+  describe "admin attribute" do
+    
+    before(:each) do
+      @user = User.create!(@attr)
+    end
+      
+    it "should respond to admin" do
+      @user.should respond_to(:admin)
+    end
+    
+    it "should not be an admin by default" do
+      @user.should_not be_admin
+    end
+    
+    it "should be convertible to an admin" do
+      @user.toggle!(:admin)
+      @user.should be_admin
+    end
+    
+    
+    
+    
+    
+    
+    
+    
+  end
+  
+  
+  
 end
   
 
@@ -152,5 +183,6 @@ end
 #  updated_at         :datetime        not null
 #  encrypted_password :string(255)
 #  salt               :string(255)
+#  admin              :boolean         default(FALSE)
 #
 
